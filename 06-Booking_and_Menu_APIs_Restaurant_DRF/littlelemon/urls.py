b2d5535b98e02d06ab5c8c70b0f9e1f66de97548
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-04-21 11:26:07 +0200

    Update urls.py

--- 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/urls.py
+++ 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 from restaurant.views import intro
 # from rest_framework.authtoken.views import obtain_auth_token
 
