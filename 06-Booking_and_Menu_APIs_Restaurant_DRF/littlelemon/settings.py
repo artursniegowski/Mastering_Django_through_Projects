64e2d7b63e0bd93c9fa78db24db2dbe3173d15b4
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-04-30 10:27:04 +0200

    Update settings.py

--- 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/settings.py
+++ 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
 ## <- ##
