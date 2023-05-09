f176c305cf61ee4da6523e7d5d87c607269dbcf0
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-05-09 09:12:09 +0200

    Update script.js

--- 05-Booking-System-Django-AJAX/app_booking/static/app_booking/js/script.js
+++ 05-Booking-System-Django-AJAX/app_booking/static/app_booking/js/script.js
@@ -3,7 +3,7 @@
 const formatTimeToAMPM = (time) => {
     const ampm = time < 12 ? 'AM' : 'PM';
     const t = time < 12 ? time : time > 12 ? time - 12 : time;
-    const label = `${t} ${ampm}`;
+    const label = `${t} ${ampm}`; 
     return label;
 }; 
 
