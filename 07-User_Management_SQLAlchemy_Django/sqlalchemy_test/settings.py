8a6bb2a804021a49453e7a6a511af9dfa1da629f
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-05-15 09:08:38 +0200

    Update settings.py

--- 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
+++ 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'PASSWORD':os.environ.get('MYSQL_PASSWORD'),
         'HOST':os.environ.get('MYSQL_DATABASE_HOST','127.0.0.1'),
         'PORT':os.environ.get('MYSQL_DATABASE_PORT', '3306'),
-        'OPTIONS': {
+        'OPTIONS': { 
             'charset': 'utf8mb4', # for more charactersets like emojis
         },
         # django requires the engine to be defined , so i migth as well use the engine 
