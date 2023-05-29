f2308625ebfbbda3097405cf60a6031edb99b2e6
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-05-29 12:09:17 +0200

    Update settings.py

--- 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/settings.py
+++ 06-Booking_and_Menu_APIs_Restaurant_DRF/littlelemon/settings.py
@@ -18,7 +18,7 @@ import os
 # NOT NEEDED IN PRODUCTION
 from dotenv import load_dotenv
 load_dotenv() 
-##########################################################################
+########################################################################## 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
