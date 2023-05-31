f8db61a11de24d3d1f5c02494665ac270c2d294f
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-06-01 00:40:41 +0200

    Update urls.py

--- 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/urls.py
+++ 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include('app_add_models.urls')), 
-    # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls), 
 ]
