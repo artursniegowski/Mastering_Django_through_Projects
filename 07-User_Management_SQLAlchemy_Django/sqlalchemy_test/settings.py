e5e87ac9fbdba3503182338da23ec03f4d24cb58
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-06-17 11:06:23 +0200

    Update settings.py

--- 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
+++ 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 ########################################################################## 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
