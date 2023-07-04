11d08820d8a6e1a12438553317b0ce407afc1ecb
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-07-04 11:34:33 +0200

    Update settings.py

--- 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
+++ 07-User_Management_SQLAlchemy_Django/sqlalchemy_test/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'sqlalchemy_test.wsgi.application'
 # connection fro sqlalchemy 
 MYSQL_URI = f"mysql+pymysql://{os.environ.get('MYSQL_USER', 'root')}:{os.environ.get('MYSQL_PASSWORD')}@{os.environ.get('MYSQL_DATABASE_HOST', '127.0.0.1')}:{os.environ.get('MYSQL_DATABASE_PORT', '3306')}/{os.environ.get('MYSQL_DATABASE_NAME')}"
 
-DATABASES = {
+DATABASES = { 
     'default': {
         'NAME':os.environ.get('MYSQL_DATABASE_NAME'),
         'USER':os.environ.get('MYSQL_USER','root'),
