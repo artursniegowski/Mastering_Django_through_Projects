4738e540f1f658fe9281a4d388d8736885e32cb9
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-07-21 11:30:40 +0200

    Update models.py

--- 08-FundProjects_Django/app_users/models.py
+++ 08-FundProjects_Django/app_users/models.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import AbstractUser
 # in this example 
 # https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
 class User(AbstractUser):
-    """my custom user - that inherits from django user evertyhing
+    """my custom user - that inherits from django user evertyhing 
     and only adding the client relationship"""  
     # adding a relationship, each client can have multiple users 
     # one - to many 
