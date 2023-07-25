2e5050357e1f370b5d69e6cce8dc0ae33a4d9cac
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-07-25 13:26:31 +0200

    Update context_processors.py

--- 08-FundProjects_Django/FunctionFunding/context_processors.py
+++ 08-FundProjects_Django/FunctionFunding/context_processors.py
@@ -3,6 +3,6 @@ import datetime
 # this context needs to be registered in settings.py
 def get_current_year_context(request):
     current_year = datetime.datetime.now().year
-    return {
+    return { 
         'current_year': current_year
     }
