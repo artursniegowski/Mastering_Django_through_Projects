ffd41cf2f42c6fe050e871bc90d22368de3399a9
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-07-27 22:07:41 +0200

    Update models.py

--- 08-FundProjects_Django/app_clients/models.py
+++ 08-FundProjects_Django/app_clients/models.py
@@ -6,6 +6,6 @@ class Client(models.Model):
     name = models.CharField(max_length=250,unique=True, blank=False)
     descritpion = models.TextField(max_length=2000, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self) -> str: 
         return self.company 
  
