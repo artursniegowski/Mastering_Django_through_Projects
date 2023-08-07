4ad876742957c67ac63bdc8b329d1db1927259de
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-08-07 15:14:50 +0200

    Update models.py

--- 08-FundProjects_Django/app_users/models.py
+++ 08-FundProjects_Django/app_users/models.py
@@ -12,6 +12,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """my custom user - that inherits from django user evertyhing 
     and only adding the client relationship"""  
-    # adding a relationship, each client can have multiple users 
+    # adding a relationship, each client can have multiple users  
     # one - to many 
     client = models.ForeignKey('app_clients.Client', on_delete=models.SET_NULL, null=True, blank=True, related_name='clients')  
