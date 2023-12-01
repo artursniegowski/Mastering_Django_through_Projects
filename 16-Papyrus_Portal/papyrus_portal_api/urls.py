a13c54091e88072894000cf03d2aff01c8f4a3a1
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-12-01 10:54:23 +0100

    Update urls.py

--- 16-Papyrus_Portal/papyrus_portal_api/urls.py
+++ 16-Papyrus_Portal/papyrus_portal_api/urls.py
@@ -18,7 +18,7 @@ from dj_rest_auth.views import PasswordResetConfirmView
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
+from drf_yasg import openapi 
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
