3c9e7953ceadb11f3397730c1927d9b5524337a5
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-02-08 09:34:22 +0100

    Update apps.py

--- 16-Papyrus_Portal/core_apps/bookmarks/apps.py
+++ 16-Papyrus_Portal/core_apps/bookmarks/apps.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BookmarksConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField" 
     name = "core_apps.bookmarks" 
     verbose_name = _("Bookmarks")
+    
