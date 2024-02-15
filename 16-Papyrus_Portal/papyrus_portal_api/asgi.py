de131b46db1a7065cd6987bdfea2d7f5131df443
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-02-15 12:22:39 +0100

    Update asgi.py

--- 16-Papyrus_Portal/papyrus_portal_api/asgi.py
+++ 16-Papyrus_Portal/papyrus_portal_api/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+ 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "papyrus_portal_api.settings")
 
 application = get_asgi_application()
