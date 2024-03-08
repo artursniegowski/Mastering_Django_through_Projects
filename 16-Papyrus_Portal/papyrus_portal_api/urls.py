e91bf0413cd13274ee648018ed3f64d5f2ce23a7
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-03-08 19:00:57 +0100

    Update urls.py

--- 16-Papyrus_Portal/papyrus_portal_api/urls.py
+++ 16-Papyrus_Portal/papyrus_portal_api/urls.py
@@ -12,7 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')) 
 """
 from dj_rest_auth.views import PasswordResetConfirmView
 from django.conf import settings
