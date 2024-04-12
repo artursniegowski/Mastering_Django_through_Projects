cf36a36d1e29aaf4e344e0a69c51229f80b32662
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-04-12 09:54:44 +0200

    Update __init__.py

--- 16-Papyrus_Portal/papyrus_portal_api/__init__.py
+++ 16-Papyrus_Portal/papyrus_portal_api/__init__.py
@@ -3,5 +3,5 @@ from .celery import app as celery_app
 # this is the code that will be executed when the
 # celery worker is started. It will load the django settings
 # module and then discover the tasks in the installed
-# Django apps
+# Django apps 
 __all__ = ("celery_app",) 
