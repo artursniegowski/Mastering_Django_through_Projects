d42112d8513fae653f49aa55062f39dbb67e4b8b
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-06-10 09:55:00 +0200

    Update base.py

--- 16-Papyrus_Portal/papyrus_portal_api/settings/base.py
+++ 16-Papyrus_Portal/papyrus_portal_api/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
-import environ
+import environ 
 
 # configuring enviromental variables
 env = environ.Env()
