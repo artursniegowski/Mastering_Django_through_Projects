a866e167ddc755ac641721acb89ac62c990a97b1
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2024-07-19 09:24:09 +0200

    Update apps.py

--- 20-Mastering-Celery/backend/app/celery_app/apps.py
+++ 20-Mastering-Celery/backend/app/celery_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CeleryAppConfig(AppConfig):
+class CeleryAppConfig(AppConfig): 
     default_auto_field = "django.db.models.BigAutoField"
     name = "celery_app"
